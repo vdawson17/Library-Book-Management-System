d4473fdc7e573cd6c5c6678a581eb84c02319cef
vdawson17 <vincegdawson@gmail.com>
2025-02-12 23:02:08 -0600

    Added implementation of removeBook
    also realized i put javadoc comment format for comments inside of methods so i changed that

--- main/java/project/library/Library.java
+++ main/java/project/library/Library.java
@@ -17,22 +17,43 @@ public class Library {
 	 */
 	public boolean addBook(Book book) {
 		if (book == null) {
-			return false; /**makes sure that empty object isn't passed in*/
+			return false; //makes sure that empty object isn't passed in*/
 		}
 		if (count < books.length) {
 			books[count] = book;
 			count++;
 			return true;
-			/**
-			 * adds book to end of books array
-			 */
+			//adds book to end of books array
 		}
 		else {
-			return false; 
-			/**
-			 * not room for another book
-			 */
+			return false;}
+			//not room for another book
 		}
+		
+	/**
+	 * 
+	 * @param book given book, remove book from array with matching information
+	 * @return return true if successful, false if unsuccessful
+	 */
+		public boolean removeBook(Book book) {
+			if (book ==null) {
+				return false; //can't be done if there is not book info
+			}
+			//loop through array until matching book is found
+			for (int i = 0; i<count-1; i++) {
+				if (books[i].equals(book)) {
+					//shift elements to left to fill gap
+					for (int j = i; j<count-1; j++) {
+						books[j] = books[j+1];
+					}
+					//clear info of removed book area
+					books[count-1] = null;
+					//decrease book counter
+					count--;
+					return true;
+				}
+				
+			} return false;//book not found
 	}
 	
 	
