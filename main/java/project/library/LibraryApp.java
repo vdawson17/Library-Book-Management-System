2214fd1e7e41cdbf88e3a69f88162ca86e2b1a30
vdawson17 <vincegdawson@gmail.com>
2025-02-12 23:28:35 -0600

    added javadoc documentation and implementation for adding books to array

--- main/java/project/library/LibraryApp.java
+++ main/java/project/library/LibraryApp.java
@@ -4,13 +4,23 @@ package project.library;
 * @version 23.0.2
 */
 public class LibraryApp {
+	
 	public static void main(String[] args) {
+		
+		//creates a library object
 		Library library = new Library();
 		
+		/**
+		 * create Book objects with all the attributes
+		 */
 		Book book1 = new Book("Books of Blood", "Clive Barker", "978-2-9022-4777-6", 14.99);
 		Book book2 = new Book("The Trial", "Franz Kafka","978-8-4053-9813-7", 9.99);
 		Book book3 = new Book("Persepolis", "Marjane Satrapi", "978-8-0368-9218-9", 6.99);
 		
+		//add books to array
+		library.addBook(book1);
+		library.addBook(book2);
+		library.addBook(book3);
 		
 	}
 	
