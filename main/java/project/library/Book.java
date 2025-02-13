d634c7233de37f623f8357334b2e2587a4eb2863
vdawson17 <vincegdawson@gmail.com>
2025-02-12 22:08:44 -0600

    Added variables to Book class

--- main/java/project/library/Book.java
+++ main/java/project/library/Book.java
@@ -1,5 +1,15 @@
 package project.library;
-
+/**
+ * Book class implementation that declares the title, author, ISBN, and price attributes and has constructors that initialize those attributes, set those attributes, and a constructor that creates a new Book object.
+ * @author Vincent Dawson
+ * @version 23.0.2
+ */
 public class Book {
+	
+	/**
+	 *  Variables representing the title, author, ISBN, and price of the books.
+	 */
+	private String title, author, ISBN;
+	private double price;
 
 }
