2a8ea6f8455822918cc809593a41818ba494da85
vdawson17 <vincegdawson@gmail.com>
2025-02-12 22:47:02 -0600

    Beginning of implementation of Library class
    includes general info, books array, counter, and start of addBook implementation

--- main/java/project/library/Library.java
+++ main/java/project/library/Library.java
@@ -1,5 +1,24 @@
 package project.library;
-
+/**
+ * Library class implementation that creates an array of books and a counter for them. It also allows adding or removing to the array, searching through the array, and printing the details of the books.
+ * @author Vincent Dawson
+ * @version 23.0.2
+ */
 public class Library {
+	/**
+	 * Declaration of Book array and counter for array.
+	 */
+	private Book[] books = new Book[5];
+	private int count = 0;
+	
+	public boolean addBook(Book book) {
+		if (book == null) {
+			return false;
+		}
+	}
+	
+	
+	
+	
 
 }
