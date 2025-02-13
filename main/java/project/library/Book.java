229da1835dcd1c82f36764c4cab0564d4b5094a5
vdawson17 <vincegdawson@gmail.com>
2025-02-12 22:21:26 -0600

    Add copy constructor and default values to initialize Book attributes

--- main/java/project/library/Book.java
+++ main/java/project/library/Book.java
@@ -11,5 +11,31 @@ public class Book {
 	 */
 	private String title, author, ISBN;
 	private double price;
+	
+	/**
+	 * Adds default values for the variables.
+	 */
+	public Book() {
+		title = "unknown";
+		author = "unknown";
+		ISBN = "unknown";
+		price = 0.0;
+	}
+	/**
+	 * @param copy Accepts a book object and creates a new one.
+	 */
+	public Book(Book copy) {
+		if (copy == null) {
+			System.out.println("Error, empty Book object passed in."); 
+			System.exit(0); /*exits program if empty Book passed in*/
+		}
+		
+		this.title = copy.title;
+		this.author = copy.author;
+		this.ISBN = copy.ISBN;
+		
+		this.price = copy.price;
+	}
+	
 
 }
